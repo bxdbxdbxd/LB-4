05cee44e594e9d7037cb569a8e5a6128d3d42fe7
Time:D <darya.sam54@gmail.com>
2024-05-30 22:24:52 +0700

    Fin ver points 1-3

--- var2-task1.cpp
+++ var2-task1.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     int len_arr = 15, sum_posit = 0, sum_negat = 0, qua_posit = 0, qua_negat = 0;
     int array_of_num[15], mx_num = -100, mn_num = 100, ind_mn, ind_mx;
+    ranlux24 engine; // Инициализация генератора Ranlux24
+    uniform_int_distribution<int> dist(-100, 100);
+
     for (int i = 0; i < len_arr; i++) {
-        array_of_num[i] = rand() % (100 + 100 + 1) - 100;
+        array_of_num[i] = dist(engine);
         if (array_of_num[i] < 0) {
             sum_negat += array_of_num[i];
             qua_negat += 1;
@@ -32,6 +37,11 @@ int main() {
     for (int i = 0; i < len_arr; i++) {
         cout << array_of_num[i] << " ";
     }
-    cout << "\n" << sum_posit / qua_posit << "+" << sum_negat / qua_negat << "-";
+    cout << "\nArithmetic mean of positive  " << sum_posit / qua_posit << "\nArithmetic mean of negative  " << sum_negat / qua_negat << endl;
+    sort(array_of_num, array_of_num + len_arr, greater<int>());
+
+    for (int i = 0; i < len_arr; i++) {
+        cout << array_of_num[i] << " ";
+    }
     return 0;
 }
