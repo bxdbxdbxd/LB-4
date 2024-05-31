92acf0a25b0640398a48b701fc73d20bb7d1dd8c
darya.sam54@gmail.com <99540593+bxdbxdbxd@users.noreply.github.com>
2024-05-31 12:51:11 +0700

    Fin vet task 3

--- var7.cpp
+++ var7.cpp
@@ -19,59 +19,21 @@ private:
     unsigned int seed;
 };
 
-// Заполнение массива случайными числами от 1 до 100
-void fillArray(vector<int>& array, int size) {
-    Ranlux24 rng(time(0)); // Инициализация генератора случайных чисел с текущим временем в качестве начального значения
+void fillArray(vector<int>& array, int size);
+double chiSquareTest(const vector<int>& array, int numBins);
 
-    for (int i = 0; i < size; i++) {
-        array[i] = (rng() % 100) + 1; // Генерация случайного числа от 1 до 100
-    }
-}
-
-// Расчет критерия хи-квадрат для проверки нормальности распределения
-double chiSquareTest(const vector<int>& array, int numBins) {
-    // Количество элементов в каждом интервале
-    vector<int> expected(numBins, 0);
-    vector<int> observed(numBins, 0);
-
-    // Равномерное распределение значений в интервалах
-    for (int i = 0; i < numBins; i++) {
-        expected[i] = array.size() / numBins;
-    }
-
-    // Подсчет количества элементов в каждом интервале
-    for (int e : array) {
-        int bin = (e - 1) / (100 / numBins);
-        observed[bin]++;
-    }
-
-    // Расчет критерия хи-квадрат
-    double chiSquare = 0.0;
-    for (int i = 0; i < numBins; i++) {
-        chiSquare += pow((observed[i] - expected[i]), 2) / expected[i];
-    }
-
-    return chiSquare;
-}
 
 int main() {
     setlocale(LC_ALL, "rus");
-    // Размеры массивов
     const int sizes[] = {50, 100, 1000};
-
-    // Количество интервалов для критерия хи-квадрат
     const int numBins = 10;
 
     for (int size : sizes) {
-        // Заполнение массива случайными числами
         vector<int> array(size);
         fillArray(array, size);
-
-        // Расчет критерия хи-квадрат
         double chiSquare = chiSquareTest(array, numBins);
 
-        // Определение значимости критерия хи-квадрат
-        double criticalValue = 16.92; // Критическое значение хи-квадрат для 9 степеней свободы и уровня значимости 0,05
+        double criticalValue = 16.92;
         wcout << L"Критерий хи-квадрат для массива размером " << size << ": " << chiSquare << endl;
         if (chiSquare < criticalValue) {
             wcout << L"Гипотеза о нормальном распределении не отвергается." << endl;
@@ -82,4 +44,30 @@ int main() {
     }
 
     return 0;
+}
+
+// Расчет критерия хи-квадрат для проверки нормальности распределения
+double chiSquareTest(const vector<int>& array, int numBins) {
+    vector<int> expected(numBins, 0);
+    vector<int> observed(numBins, 0);
+    for (int i = 0; i < numBins; i++) {
+        expected[i] = array.size() / numBins;
+    }
+    for (int e : array) {
+        int bin = (e - 1) / (100 / numBins);
+        observed[bin]++;
+    }
+    double chiSquare = 0.0;
+    for (int i = 0; i < numBins; i++) {
+        chiSquare += pow((observed[i] - expected[i]), 2) / expected[i];
+    }
+
+    return chiSquare;
+}
+
+void fillArray(vector<int>& array, int size) {
+    Ranlux24 rng(time(0));
+    for (int i = 0; i < size; i++) {
+        array[i] = (rng() % 100) + 1;
+    }
 }
